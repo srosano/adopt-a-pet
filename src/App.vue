4615dea286f4886b3c48e4b81a2f7ce017bac909
scott rosano <scott.rosano@gmail.com>
2022-01-19 16:48:46 +1100

    adopt-pet done in my spare time https://www.youtube.com/watch?v=4deVCNJq3qc

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/cats">Cats</router-link> |
+      <router-link to="/dogs">Dogs</router-link> |
     </div>
-    <router-view/>
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/data/dogs.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    species: 'dog',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    lodogion: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    species: 'dog',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    lodogion: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Zeus',
+    breed: 'afghan hound',
+    species: 'dog',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    lodogion: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Katie',
+    breed: 'golden retriever',
+    species: 'dog',
+    gender: 'female',
+    age: 2,
+    color: 'black',
+    weight: 44,
+    lodogion: 'threed',
+    notes: 'Super friendly'
+  }
+]
